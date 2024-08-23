6207b8ddf2bbf83bb0bab747e1bcae1322ab856d
mskovv <maslay99@gmail.com>
2024-08-23 14:27:53 +0500

    init

---
+++ fast.go
@@ -0,0 +1,289 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/mailru/easyjson"
+	"hw3/model"
+	"io"
+	"os"
+	"strings"
+)
+
+//func FastSearch(out io.Writer) {
+//	file, err := os.Open(filePath) //пробуем открыть файл
+//	if err != nil {
+//		panic(err)
+//	}
+//
+//	fileContents, err := io.ReadAll(file) //пробуем читать все содержимое файла сразу (грузим все в МЕМ)
+//	if err != nil {
+//		panic(err)
+//	}
+//
+//	var seenBrowsers []string
+//	uniqueBrowsers := 0
+//	foundUsers := ""
+//	buf := bytes.Buffer{}
+//
+//	lines := strings.Split(string(fileContents), "\n")
+//
+//	users := make([]map[string]interface{}, 0, len(lines))
+//	for _, line := range lines {
+//		user := make(map[string]interface{})
+//		// fmt.Printf("%v %v\n", err, line)
+//		err := json.Unmarshal([]byte(line), &user)
+//		if err != nil {
+//			if err == io.EOF {
+//				break
+//			}
+//			panic(err)
+//		}
+//		users = append(users, user)
+//	}
+//
+//	for i, user := range users {
+//
+//		isAndroid := false
+//		isMSIE := false
+//
+//		browsers, ok := user["browsers"].([]interface{})
+//		if !ok {
+//			// log.Println("cant cast browsers")
+//			continue
+//		}
+//
+//		for _, browserRaw := range browsers {
+//			browser, ok := browserRaw.(string)
+//			if !ok {
+//				// log.Println("cant cast browser to string")
+//				continue
+//			}
+//			if strings.Contains(browser, "Android") {
+//				isAndroid = true
+//				notSeenBefore := true
+//				for _, item := range seenBrowsers {
+//					if item == browser {
+//						notSeenBefore = false
+//					}
+//				}
+//				if notSeenBefore {
+//					seenBrowsers = append(seenBrowsers, browser)
+//					uniqueBrowsers++
+//				}
+//			} else if strings.Contains(browser, "MSIE") {
+//				isMSIE = true
+//				notSeenBefore := true
+//				for _, item := range seenBrowsers {
+//					if item == browser {
+//						notSeenBefore = false
+//					}
+//				}
+//				if notSeenBefore {
+//					seenBrowsers = append(seenBrowsers, browser)
+//					uniqueBrowsers++
+//				}
+//			}
+//		}
+//
+//		if !(isAndroid && isMSIE) {
+//			continue
+//		}
+//
+//		email := strings.ReplaceAll(user["email"].(string), "@", " [at] ")
+//		buf.WriteString(fmt.Sprintf("[%d] %s <%s>\n", i, user["name"], email))
+//	}
+//
+//	foundUsers = buf.String()
+//	_, err = fmt.Fprintln(out, "found users:\n"+foundUsers)
+//	if err != nil {
+//		return
+//	}
+//	_, err = fmt.Fprintln(out, "Total unique browsers", len(seenBrowsers))
+//	if err != nil {
+//		return
+//	}
+//}
+
+//func FastSearch(out io.Writer) {
+//	file, err := os.Open(filePath) // пробуем открыть файл
+//	if err != nil {
+//		panic(err)
+//	}
+//	defer file.Close() // закрываем файл после использования
+//
+//	fileContents, err := io.ReadAll(file)
+//	if err != nil {
+//		panic(err)
+//	}
+//
+//	var seenBrowsers []string
+//	uniqueBrowsers := 0
+//	foundUsers := ""
+//	buf := bytes.Buffer{}
+//
+//	users := make([]map[string]interface{}, 0, cap(fileContents)/4)
+//	decoder := json.NewDecoder(bytes.NewReader(fileContents))
+//	for {
+//		user := make(map[string]interface{})
+//		err := decoder.Decode(&user)
+//		if err != nil {
+//			if err == io.EOF {
+//				break
+//			}
+//			panic(err)
+//		}
+//		users = append(users, user)
+//	}
+//
+//	for i, user := range users {
+//		isAndroid := false
+//		isMSIE := false
+//
+//		browsers, ok := user["browsers"].([]interface{})
+//		if !ok {
+//			// log.Println("cant cast browsers")
+//			continue
+//		}
+//
+//		for _, browserRaw := range browsers {
+//			browser, ok := browserRaw.(string)
+//			if !ok {
+//				// log.Println("cant cast browser to string")
+//				continue
+//			}
+//			if strings.Contains(browser, "Android") {
+//				isAndroid = true
+//				notSeenBefore := true
+//				for _, item := range seenBrowsers {
+//					if item == browser {
+//						notSeenBefore = false
+//					}
+//				}
+//				if notSeenBefore {
+//					seenBrowsers = append(seenBrowsers, browser)
+//					uniqueBrowsers++
+//				}
+//			} else if strings.Contains(browser, "MSIE") {
+//				isMSIE = true
+//				notSeenBefore := true
+//				for _, item := range seenBrowsers {
+//					if item == browser {
+//						notSeenBefore = false
+//					}
+//				}
+//				if notSeenBefore {
+//					seenBrowsers = append(seenBrowsers, browser)
+//					uniqueBrowsers++
+//				}
+//			}
+//		}
+//
+//		if !(isAndroid && isMSIE) {
+//			continue
+//		}
+//
+//		email := strings.ReplaceAll(user["email"].(string), "@", " [at] ")
+//		buf.WriteString(fmt.Sprintf("[%d] %s <%s>\n", i, user["name"], email))
+//	}
+//
+//	foundUsers = buf.String()
+//	_, err = fmt.Fprintln(out, "found users:\n"+foundUsers)
+//	if err != nil {
+//		return
+//	}
+//	_, err = fmt.Fprintln(out, "Total unique browsers", len(seenBrowsers))
+//	if err != nil {
+//		return
+//	}
+//}
+
+func FastSearch(out io.Writer) {
+	file, err := os.Open(filePath) // пробуем открыть файл
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close() // закрываем файл после использования
+
+	fileContents, err := io.ReadAll(file)
+	if err != nil {
+		panic(err)
+	}
+
+	var seenBrowsers []string
+	uniqueBrowsers := 0
+	foundUsers := ""
+	buf := bytes.Buffer{}
+
+	lines := strings.Split(string(fileContents), "\n")
+
+	users := make([]model.User, 0)
+	//users := make([]map[string]interface{}, 0)
+	for _, line := range lines {
+		user := model.User{}
+		// fmt.Printf("%v %v\n", err, line)
+		//err := json.Unmarshal([]byte(line), &user)
+		err := easyjson.Unmarshal([]byte(line), &user)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			panic(err)
+		}
+		users = append(users, user)
+	}
+
+	for i, user := range users {
+		isAndroid := false
+		isMSIE := false
+
+		browsers := user.Browsers
+
+		for _, browserRaw := range browsers {
+			browser := browserRaw
+
+			if strings.Contains(browser, "Android") {
+				isAndroid = true
+				notSeenBefore := true
+				for _, item := range seenBrowsers {
+					if item == browser {
+						notSeenBefore = false
+					}
+				}
+				if notSeenBefore {
+					seenBrowsers = append(seenBrowsers, browser)
+					uniqueBrowsers++
+				}
+			} else if strings.Contains(browser, "MSIE") {
+				isMSIE = true
+				notSeenBefore := true
+				for _, item := range seenBrowsers {
+					if item == browser {
+						notSeenBefore = false
+					}
+				}
+				if notSeenBefore {
+					seenBrowsers = append(seenBrowsers, browser)
+					uniqueBrowsers++
+				}
+			}
+		}
+
+		if !(isAndroid && isMSIE) {
+			continue
+		}
+
+		email := strings.ReplaceAll(user.Email, "@", " [at] ")
+		buf.WriteString(fmt.Sprintf("[%d] %s <%s>\n", i, user.Name, email))
+	}
+
+	foundUsers = buf.String()
+	_, err = fmt.Fprintln(out, "found users:\n"+foundUsers)
+	if err != nil {
+		return
+	}
+	_, err = fmt.Fprintln(out, "Total unique browsers", len(seenBrowsers))
+	if err != nil {
+		return
+	}
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module hw3
+
+go 1.3
+
+require github.com/mailru/easyjson v0.7.7 // indirect
